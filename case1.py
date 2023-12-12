64a46f8a582acf248056cca4681fc50a4ea106d4
kliea <klinthjeraldqt@gmail.com>
2023-12-13 07:19:17 +0800

    added population function

--- case1.py
+++ case1.py
@@ -4,7 +4,7 @@ import numpy as np
 num_units = 7
 num_intervals = 4
 max_loads = [80, 90, 65, 70]
-totalcap = 0  # Initialize totalcap
+totalcap = 150  # Initialize totalcap
 
 # Define the GA parameters
 pop_size = 10
@@ -22,54 +22,64 @@ unitData = np.array([
     (10, 1)
 ])
 
-#generate a chromosome
-def generateGenome(unitData, num_intervals, max_loads, totalcap):
-    #generate empty chromosome
+# Generate a chromosome
+def generateGenome(unitData, num_intervals):
+    # Generate an empty chromosome
     chromosome = np.zeros((num_units, num_intervals), dtype=int)
-    
-      
+
     for unit in range(num_units):
         if unitData[unit][1] > num_intervals:
-            raise ValueError("Unit's required interval must not be greater than load interval")
+            raise ValueError("Unit's required interval must not be greater than the load interval")
 
-        # maintenance of any unit starts at the beginning of an interval and finishes at the same or adjacent interval. example required interval for unit = 2 then [0 0 1 1] [1 1 0 0 ] [0 1 1 0]    
+        # Maintenance of any unit starts at the beginning of an interval and finishes at the same or adjacent interval.
         startInterval = np.random.randint(0, num_intervals - unitData[unit][1] + 1)
 
         for interval in range(startInterval, startInterval + unitData[unit][1]):
-            chromosome[unit, interval] = 1    
-        
-        #define total capacity
-        totalcap = totalcap + unitData[unit][0]   
-    
-    return chromosome, totalcap
+            chromosome[unit, interval] = 1
 
-#generate chromosome
-chromosome, totalcap = generateGenome(unitData, num_intervals, max_loads, totalcap)
+    return chromosome
 
-#calculate fitness of the chromosome
-def fitness(chromosome, unitData, max_loads, totalcap):
-    # Reshape unitData[:,0] to match the shape of chromosome
-    unit_capacity = unitData[:,0].reshape(-1, 1)
-    
-    # Calculate the total capacity for each interval
-    installed_capacity = np.sum(chromosome * unit_capacity, axis=0)
-    
-    # Calculate the difference between the total capacity and the maximum loads
-    total_installed_capacity = np.abs(totalcap - installed_capacity)
-    
-    # Calculate net reserves
-    net_reserves = total_installed_capacity - max_loads
+# Generate a chromosome
+# chromosome = generateGenome(unitData, num_intervals)
 
-    # If net_reserves is negative, set it to 0
-    net_reserves = np.maximum(0, net_reserves)
-    print("Net Reserves: ", str(net_reserves))
+# Calculate the fitness of the chromosome
+def calculate_fitness(chromosome, unitData, num_intervals, max_loads, totalcap):
+    net_reserves = np.zeros(num_intervals, dtype=int)
+    numChromosome = len(chromosome)
 
-    # define the fitness value is the lowest net reserve    
+    for col in range(num_intervals):
+        totalLoad = 0
+
+        # totalLoad is the sum of capacities of the units scheduled for maintenance at each interval
+        for row in range(numChromosome):
+            totalLoad += (chromosome[row, col] * unitData[row][0])
+
+        net_reserves[col] = totalcap - totalLoad - max_loads[col]
+
+    # if the net reserve at any interval is negative, the schedule is illegal, and the fitness function returns zero
+    if any(net_reserves < 0):
+        return 0
+    
+    # define the fitness value as the lowest net reserve    
     return np.min(net_reserves)
 
-fitness = fitness(chromosome, unitData, max_loads, totalcap)
+# Generate Fitness of the chromosome
+# fitness = calculate_fitness(chromosome, unitData, num_intervals, max_loads, totalcap)
 
-print(f"Generated chromosome: ")
-print(chromosome)
-print ("Fitness: "+ str(fitness))
+def initialize_population(pop_size, unitData, num_intervals, max_loads, totalcap):
+    population = []
+    for _ in range(pop_size):
+        chromosome = generateGenome(unitData, num_intervals)
+        fitness_value = calculate_fitness(chromosome, unitData, num_intervals, max_loads, totalcap)
+        population.append({"chromosome": chromosome, "fitness": fitness_value})
+    return population
 
+# Generate the population
+population = initialize_population(pop_size, unitData, num_intervals, max_loads, totalcap)
+
+for i, individual in enumerate(population):
+    print(f"Individual {i + 1}:")
+    print("Chromosome:")
+    print(individual["chromosome"])
+    print("Fitness:", individual["fitness"])
+    print()
